815307c22fcde6923c995dcbc76434a5533bbbff
khanhdeptraine <145850265+khanhdeptraine@users.noreply.github.com>
2024-09-17 17:42:49 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import { Routes, Route } from 'react-router-dom';
 import DoctorLoginForm from './doctorLoginForm';
 import ProfileDoctor from './profileDoctor';
 import UpdateDoctor from './UpdateProfilePage';
-// import HosoPage from './HosoPage';
+import HosoPage from './HosoPage';
+import ProfilePatient from './profilePatient'; // Ensure this import matches the file name
+import AddPatientRecord from './AddPatientRecord';
+import NewRecord from './newRecord';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<DoctorLoginForm />} />
-      {/* <Route path="/hoso/:username" element={<HosoPage />} /> */}
+      <Route path="/hoso/:username" element={<HosoPage />} />
       <Route path="/profileDoctor/:username" element={<ProfileDoctor />} />
       <Route path="/updateDoctor/:username" element={<UpdateDoctor />} />
+      <Route path="/profilePatient/:patientAddress" element={<ProfilePatient />} /> {/* Add this route */}
+      <Route path="/addpatientrecord/:username" element={<AddPatientRecord />} />
+      <Route path="/newRecord" element={<NewRecord />} />
     </Routes>
   );
 }
